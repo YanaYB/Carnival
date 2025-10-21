bc4fcde2f210933453f745a2df914d4088eab7bb
YanaYB <acolaserena@gmail.com>
2025-10-21 14:26:41 +0300

    2

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,24 +1,8 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform player;
-    public Vector3 offset;
-    private float fixedY; // фиксированная высота камеры
 
-    void Start()
-    {
-        offset = transform.position - player.position;
-        //fixedY = transform.position.y; // запоминаем начальную высоту
-    }
-
-    void LateUpdate()
-    {
-        if (player != null)
-        {
-            Vector3 targetPosition = player.position + offset;
-            //targetPosition.y = fixedY; // оставляем Y неизменным
-            transform.position = targetPosition;
-        }
-    }
+   
 }
